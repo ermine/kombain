7acfca05becb58fdceb6853006adb0dcf17384bb
Anastasia Gornostaeva <ermine@sulci.ru>
2012-06-16 17:58:13 +0400

    BBCode: Almost full parser and converter to html

---
+++ bbcode/bbcode2html.ml
@@ -0,0 +1,295 @@
+open Bbcode_parser
+open Html5.M
+open Html5.P
+
+let _ = Printexc.record_backtrace true
+
+let rec make_phrasing is =
+  List.fold_right (fun t acc ->
+    match t with
+      | Abbr (is, value) -> abbr ~a:[a_title value] (make_phrasing is) :: acc
+      | Strong is -> strong (make_phrasing is) :: acc
+      | Em is -> em (make_phrasing is) :: acc
+      | Strike is ->
+        span ~a:[a_style "text-decoration:line-through"]
+          (make_phrasing is) :: acc
+      | Subscript is -> sub (make_phrasing is) :: acc
+      | Superscript is -> sup (make_phrasing is) :: acc
+      | Underline is ->
+        span ~a:[a_style "text-decoration:underline"] (make_phrasing is) :: acc
+      | Color (value, is) ->
+        span ~a:[a_style ("color:"^value)] (make_phrasing is) :: acc
+      | Font (value, is) ->
+        span ~a:[a_style ("font-family:"^value)] (make_phrasing is) :: acc
+      | Sp -> space () :: acc
+      | Code str -> (code [pcdata str]) :: acc
+      | Image data ->
+        let a = if data.align <> "" then
+            [a_style ("float:" ^ data.align)]
+          else
+            []
+        in
+        let a =
+          match data.dim with
+            | None -> a
+            | Some (width, height) -> a_width width :: a_height height :: a
+        in
+          img ~a ~src:data.src ~alt:(if data.alt = "" then "[IMG]" else data.alt)
+            () :: acc
+      | Break -> br () :: acc
+      | Size (value, is) ->
+        let size = if value < 6 then 6 else if value > 48 then 48 else value in
+          span ~a:[a_style ("font-size:"^string_of_int size ^ "px")]
+            (make_phrasing is)  :: acc
+      | Anchor (anchor, is) ->
+        span ~a:[a_id anchor] (make_phrasing is) :: acc
+      | Entity e -> entity e :: acc
+      | Str str ->
+        pcdata str :: acc
+      | Float (align, is) ->
+        span ~a:[a_style ("float:" ^ align)] (make_phrasing is) :: acc
+      | NewLine -> acc
+      | _ -> assert false
+  ) is []
+
+let rec inline_to_html is =
+  List.fold_right (fun t acc ->
+    match t with
+      | Email (email, name) ->
+        (a ~a:[a_href ("mailto:" ^ email)]
+           [(match name with None -> pcdata email | Some v -> pcdata v)] :
+           Html5_types.phrasing elt
+        ) :: acc
+      | SimpleTag (key, content) -> (
+        match key with
+          | "google" ->
+            (a ~a:[a_href ("http://www.google.com/search?q=" ^ content)]
+               [pcdata content]) :: acc
+          | "wikipedia" ->
+            (a ~a:[a_href ("http://www.wikipedia.org/wiki/" ^ content)]
+               [pcdata content]) :: acc
+          | "youtube" ->
+            (object_
+               ~params:[param ~a:[a_name "movie";
+                                  a_text_value ("http://www.youtube.com/v/" ^ content)]
+                           ()]
+               ~a:[a_width 425; a_height 366]
+               [embed ~a:[a_src ("http://www.youtube.com/v/" ^ content);
+                          a_mime_type "application/x-shockwave-flash";
+                          a_width 425; a_height 366] ()]) :: acc
+          | _ -> failwith "unknown simpletag"
+      )
+      | Url (url, text) ->
+        let url =
+          if (String.length url > 7 && String.sub url 0 7 = "http://") ||
+            (String.length url > 6 && String.sub url 0 6 = "ftp://") ||
+            (String.length url > 8 && String.sub url 0 8 = "https://") then
+            url
+          else if (String.length url > 4 && String.sub url 0 4 = "ftp.") then
+            "ftp://" ^ url
+          else
+            "http://" ^ url in
+          (a ~a:[a_href url] [pcdata text]) :: acc
+      | Flash ((w, h), url) ->
+        (object_~params:[param ~a:[a_name "movie"; a_text_value url] ()]
+           ~a:[a_data url; a_width w; a_height h;
+               a_mime_type "application/x-shockwave-flash"] []) :: acc
+      | other -> (make_phrasing [other] :> Html5_types.phrasing elt list) @ acc
+  ) is []
+    
+let make_hn = function
+  | 1 -> h1
+  | 2 -> h2
+  | 3 -> h3
+  | 4 -> h4
+  | 5 -> h5
+  | 6 -> h6
+  | _ -> h6
+    
+let replace_bad_chars str =
+  let len = String.length str in
+  let str = String.copy str in
+  let rec aux_replace i =
+    if i < len then
+      match str.[i] with
+        | 'a' .. 'z' | 'A'..'Z' | '0'..'9' | '_' | '-' ->
+          aux_replace (succ i)
+        | _ -> str.[i] <- '_'; aux_replace (succ i)
+    else
+      str
+  in
+    aux_replace 0
+      
+let rec make_heading_anchor is =
+  let str =
+    List.fold_right (fun t acc ->
+      match t with
+        | Abbr (is, value) -> make_heading_anchor is :: acc
+        | Em is -> make_heading_anchor is :: acc
+        | Code str -> replace_bad_chars str :: acc
+        | Image img -> acc
+        | Flash _ -> acc
+        | Strong is -> make_heading_anchor is :: acc
+        | Subscript is -> make_heading_anchor is :: acc
+        | Superscript is -> make_heading_anchor is :: acc
+        | Entity e -> acc
+        | Str str -> replace_bad_chars str :: acc
+        | _ -> acc
+    ) is [] in
+    String.concat "" str
+
+let rec collect_inlines acc = function
+  | [] -> false, List.rev acc, []
+  | Code str as t :: rest ->
+    if String.contains str '\n' then
+      if acc = [] then
+        false, [Code str], rest
+      else
+        false, List.rev acc, (t :: rest)
+    else
+      collect_inlines (t :: acc) rest
+  | (Strong _ | Em _ | Underline _ | Strike _ | Subscript _ | Superscript _
+        | Color _ | Size _ | Font _ | Url _ | Anchor _ | Image _ | Flash _
+        | Entity _ | Str _ | SimpleTag _ | Email _ | Abbr _ | Break | Sp
+        | Float _ as t) :: rest ->
+    collect_inlines (t :: acc) rest
+  | NewLine:: NewLine :: rest ->
+    true, List.rev acc, rest
+  | NewLine :: rest ->
+    false, List.rev acc, rest
+  | t :: rest ->
+    false, List.rev acc, (t :: rest)
+            
+let make_index style data =
+  let zz = if style = "ul" then ul else ol in
+  let make_link is =
+    a ~a:[a_href ("#" ^ make_heading_anchor is)]
+      (make_phrasing is :> Html5_types.flow5_without_interactive  elt list) in
+
+  let rec fold l acc = function
+    | [] -> List.rev acc, []
+    | x :: xs ->
+      match x with
+        | Heading (lev, is) ->
+          if lev = l then
+            let i = make_link is in
+            let items,rest = fold (succ l) [] xs in
+              if items = [] then
+                fold l (li [i] :: acc) rest
+              else
+                fold l (li [i; zz items] :: acc) rest
+          else if lev > l then
+            fold lev [] (x::xs)
+          else
+            List.rev acc, (x::xs)
+        | _ -> fold l acc xs
+  in
+  let rec scan_rest acc = function
+    | [] -> acc
+    | rest ->
+      let items, rest = fold 1 [] rest in
+        scan_rest (acc @ items) rest
+  in
+  let items = scan_rest [] data in
+    zz items
+
+let is_next_block = function
+  | [] -> false
+  | x :: _ ->
+    match x with
+      | Index _ | Align _ | Heading _ | HorizontalRule | List _ | Table _
+      | Indent _ | Quote _ -> true
+      | _ -> false
+
+let rec block_to_html data =
+  let rec aux_fold acc = function
+    | [] -> List.rev acc
+    | Heading (level, is) :: rest ->
+      let hn = make_hn level in
+        aux_fold (hn ~a:[a_id (make_heading_anchor is)]
+                    (inline_to_html is) :: acc) rest
+    | Indent is :: rest ->
+      aux_fold ((div (block_to_html is)) :: acc) rest
+    | HorizontalRule :: rest ->
+      aux_fold (hr () :: acc) rest
+    | Index style :: rest ->
+      let index = make_index style data in
+        aux_fold (index :: acc) rest
+    | Quote (from, is) :: rest ->
+      if from = "" then
+        aux_fold (blockquote (block_to_html is) :: acc) rest
+      else
+        aux_fold (blockquote (em [pcdata from; pcdata " wrote:"] ::
+                                block_to_html is) :: acc) rest
+    | List (st, ls) :: rest ->
+      let zz =
+        match st with
+          | "c" -> ul ~a:[a_style "list-style-type:circle"]
+          | "d" -> ul ~a:[a_style "list-style-type:disc"]
+          | "s" -> ul ~a:[a_style "list-style-type:square"]
+          | "1" -> ol ~a:[a_style "list-style-type:decimal"]
+          | "a" -> ol ~a:[a_style "list-style-type:lower-alpha"]
+          | "A" -> ol ~a:[a_style "list-style-type:upper-alpha"]
+          | "i" -> ol ~a:[a_style "list-style-type:lower-roman"]
+          | "I" -> ol ~a:[a_style "list-style-type:upper-roman"]
+          | _ -> ul ~a:[a_style "list-style-type:circle"]
+      in
+      let res = List.map (fun l -> li (block_to_html l)) ls in
+        aux_fold (zz res :: acc) rest
+    | Table (head, (color, row), rows) :: rest ->
+      let make_color color =
+        match color with
+          | None -> []
+          | Some v -> [a_style ("background-color:"^v)] in
+      let make_td col = td (block_to_html col) in
+      let thead =
+        if head = [] then None else
+          Some (thead (List.map (fun (color, row) -> 
+            tr ~a:(make_color color) (List.map make_td row)) head)) in
+      let row = tr ~a:(make_color color) (List.map make_td row) in
+      let rows =
+        List.map (fun (color, row) ->
+          tr ~a:(make_color color) (List.map make_td row)) rows in
+          aux_fold (table ~a:[a_style "border:1px solid"]
+                      ?thead row rows :: acc) rest
+    | Align (align, smth) :: rest ->
+      aux_fold ((div ~a:[a_style ("text-align:"^align)]
+                   (block_to_html smth)) :: acc) rest
+    | NewLine :: rest ->
+      aux_fold acc rest
+    | rest ->
+      let is_para, is, rest = collect_inlines [] rest in
+        match is with
+          | [Code str] -> aux_fold (pre [code [pcdata str]] :: acc) rest
+          | _ ->
+            let res =
+              if is_para then
+                p (inline_to_html is) :: acc
+              else if is_next_block rest then
+                (inline_to_html is :> Html5_types.flow5 elt list) @ acc
+              else
+                br () :: (List.rev (inline_to_html is :>
+                                      Html5_types.flow5 elt list)) @acc
+            in
+              aux_fold res rest
+  in
+    aux_fold [] data
+
+let make_html5 data =
+  html (head (title (pcdata "abc")) [])
+    (body (block_to_html data))
+      
+let () =
+  let input = Kmb_input.of_file Sys.argv.(1) in
+  let result = Bbcode_parser.parse input in
+    match result with
+      | Kmb_lib.Failed ->
+        failwith "failed to parse bbcode"
+      | Kmb_lib.Parsed (ast, rest) ->
+        Printf.printf "Parsed, remaining %S\n" (Kmb_input.get_remaining rest);
+        let outfile = Sys.argv.(2) in
+        let oc = open_out outfile in
+        let output str = output oc str 0 (String.length str) in
+           print ~output  (make_html5 ast);
+          close_out oc
+          
--- bbcode/bbcode_test.ml
+++ bbcode/bbcode_test.ml
@@ -1,12 +1,221 @@
+open Format
+open Bbcode_parser
+  
+let rec print_token = function
+  | Strong is ->
+    printf "Strong @[<v 0>";
+    List.iter print_token is;
+    printf "@]@ "
+  | Em is ->
+    printf "Em @[<v 0>";
+    List.iter print_token is;
+    printf "@]@ "
+  | Underline is ->
+    printf "Underline @[<v 0>";
+    List.iter print_token is;
+    printf "@]@ "
+  | Strike is ->
+    printf "Strike @[<v 0>";
+    List.iter print_token is;
+    printf "@]@ "
+  | Subscript is ->
+    printf "Subscript @[<v 0>";
+    List.iter print_token is;
+    printf "@]@ "
+  | Superscript is ->
+    printf "Superscript @[<v 0>";
+    List.iter print_token is;
+    printf "@]@ "    
+  | Color (str, is) ->
+    printf "Color @[<v 0>%s@ " str;
+    List.iter print_token is;
+    printf "@]@ "
+  | Size (size, is) ->
+    printf "Size @[<v 0>%d@ " size;
+    List.iter print_token is;
+    printf "@]@ "
+  | Font (face, is) ->
+    printf "Font @[<v 0>%S@ " face;
+    List.iter print_token is;
+    printf "@]@ "
+  | Url (url, text) ->
+    printf "Url @[<hov 0>%s@ %s@]@ " url text
+  | Anchor (a, is) ->
+    printf "Ahchor @[%s@ %s@ " a;
+    List.iter print_token is;
+    printf "@]"
+  | Image img ->
+    let width, height =
+      match img.dim with
+        | None -> 0, 0
+        | Some (w, h) -> w, h
+    in
+      printf
+        "Image {@[<hov 0>src=%S@ alt=%S@ align=%S@ width=%d@ height=%d}@]@ "
+        img.src img.alt img.align width height;
+  | Flash ((w, h), url) ->
+    printf "Flash (%d,%d)@ %s@ " w h url
+  | Indent ts ->
+    printf "Indent @[<v 0>";
+    List.iter print_token ts;
+    printf "@]"
+  | Entity e ->
+    printf "Entity %S@ " e
+  | Str str ->
+    printf "Str %S@ " str
+  | SimpleTag (tag, w) ->
+    printf "SimpleTag @[<hov 0>%S@ %S@]@ " tag w
+  | Email (email, name) ->
+    printf "Email @[<hov 0>%S@ %s@]@ " email
+      (match name with None -> "none" | Some v -> v)
+  | Abbr (is, value) ->
+    printf "Abbr @[<hov 0>%S@ %S@ " value;
+    List.iter print_token is;
+    printf "@]"
+  | Break ->
+    printf "Break@;"
+  | Sp ->
+    printf "Sp@;"
 
+  | Heading (level, is) ->
+    printf "Heading %d @[<v 0>" level;
+    List.iter print_token is;
+    printf "@]@;"
+  | Quote (from, is) ->
+    printf "Quote @[<hv 0>%S@ " from;
+    List.iter print_token is;
+    printf "@]@;"
+  | Code str ->
+    printf "Code %S@ " str
+  | List (t, is) ->
+    printf "List %S@[<v 0>@ " t;
+    List.iter (fun b ->
+      printf "@[<v 0>";
+      List.iter print_token b;
+      printf "@]"
+    ) is;
+    printf "@]@;"
+  | Table (hsss, (c, bss), bsss) ->
+    printf "Table@ @[<v 0>";
+    printf "@[<v 0>";
+    List.iter (fun (c, bss) ->
+      printf "Header @[<v 0>color: %s@ " (match c with None -> "" | Some v -> v);
+      List.iter (fun bs ->
+        printf "@[<v 0>";
+        List.iter print_token bs;
+        printf "@]@;"
+      ) bss;
+      printf "@]@ "
+    ) hsss;
+    printf "@]@;";
+
+    printf "@[<v 0>color: %s@ " (match c with None -> "none" | Some v -> v);
+    List.iter (fun bs ->
+      printf "@[<v 0>";
+      List.iter print_token bs;
+      printf "@]@ "
+    ) bss;
+    printf "@]@ ";
+    
+    List.iter (fun (c, bss) ->
+      printf "@[<v 0>color: %s@ " (match c with None -> "none" | Some v -> v);
+      List.iter (fun bs ->
+        printf "@[<v 0>";
+        List.iter print_token bs;
+        printf "@]@ "
+      ) bss;
+      printf "@]@ "
+    ) bsss;
+    printf "@]@;"
+  | Index style ->
+    printf "Index style=%s@;" style
+  | HorizontalRule ->
+    printf "HorizontalRule@;"
+  | Align (align, is) ->
+    printf "Align %s@[@ " align;
+    List.iter print_token is;
+    printf "@]@;"
+  | Float (v, ts) ->
+    printf "Float@ %s@[<v 0>" v;
+    List.iter print_token ts;
+    printf "@]"
+  | NewLine ->
+    printf "NewLine@;"
+
+
+let rec union_str acc = function
+  | [] -> List.rev acc
+  | Str x :: Str y :: rest ->
+    union_str acc ((Str (x ^ y)) :: rest)
+  | Str _ as x :: rest -> 
+    union_str (x :: acc) rest
+  | Entity e :: rest ->
+    union_str (Entity e:: acc) rest
+  | Strong is :: rest ->
+    union_str ((Strong (union_str [] is)) :: acc) rest
+  | Em is :: rest ->
+    union_str ((Em (union_str [] is)) ::acc) rest
+  | Underline is :: rest ->
+    union_str ((Underline (union_str[] is)) :: acc) rest
+  | Strike is :: rest ->
+    union_str ((Strike (union_str [] is)) :: acc) rest
+  | Align (v, is) :: rest ->
+    union_str ((Align (v, union_str [] is)) :: acc) rest
+  | Float (v, is) :: rest ->
+    union_str ((Float (v, union_str [] is)) :: acc) rest
+  | Size (v, is) :: rest ->
+    union_str ((Size (v, union_str [] is)) :: acc) rest
+  | Subscript is :: rest ->
+    union_str ((Subscript (union_str [] is)) :: acc) rest
+  | Superscript is :: rest ->
+    union_str ((Superscript (union_str [] is)) :: acc) rest
+  | Font (v, is) :: rest ->
+    union_str ((Font (v, union_str [] is)) :: acc) rest
+  | Color (v, is) :: rest ->
+    union_str ((Color (v, union_str [] is)) :: acc) rest
+  | Indent is :: rest ->
+    union_str ((Indent (union_str [] is)) :: acc) rest
+  | Heading (v, is) :: rest ->
+    union_str ((Heading (v, union_str [] is)) :: acc) rest
+  | Quote (v, is) :: rest ->
+    union_str ((Quote (v, union_str [] is)) :: acc) rest
+  | List (v, ls) :: rest ->
+    union_str ((List (v, List.map (union_str []) ls)) :: acc) rest
+  | Table (h, (c, r), rs) :: rest ->
+    union_str ((Table
+                  (List.map (fun (c, rows) ->
+                    (c, List.map (union_str []) rows)) h,
+                   (c, List.map (union_str []) r),
+                   List.map (fun (c, rows) ->
+                     (c, List.map (union_str []) rows)) rs)) :: acc)
+      rest
+  | (Index _ | NewLine | Image _ | Flash _ | Url _ | Code _
+        | Abbr _ | Anchor _ | SimpleTag _ | Sp | HorizontalRule
+        | Break | Email _) as t :: rest ->
+    union_str (t :: acc) rest
+    
+
+let rec remove_newlines acc = function
+  | [] -> acc
+  | (Heading _ | List _ | Table _ | Align _ | Float _ | Break | HorizontalRule
+        | Quote _ | Index _ | Indent _ as t) :: NewLine :: rest ->
+    remove_newlines acc (t :: rest)
+  | NewLine :: NewLine :: rest ->
+    remove_newlines acc (NewLine :: rest)
+  | x :: rest ->
+    remove_newlines (x :: acc) rest
+      
 let () =
-  let file = Sys.argv.(1) in
-  let input = Kmb_input.of_file file in
+  let input = Kmb_input.of_file Sys.argv.(1) in
   let result = Bbcode_parser.parse input in
     match result with
       | Kmb_lib.Failed ->
-        Printf.printf "failed"
+        failwith "failed to parse bbcode"
       | Kmb_lib.Parsed (ast, rest) ->
-        Printf.printf "Parsed\n";
-        (* List.iter print_token ast *)
-        
+        Printf.printf "Parsed, remaining %S\n" (Kmb_input.get_remaining rest);
+        let data =
+          remove_newlines [] (List.rev (union_str [] ast)) in
+          printf "@[<v 0>";
+          List.iter print_token data;
+          printf "@]"
+          
